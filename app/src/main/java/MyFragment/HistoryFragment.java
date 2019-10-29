c7244198a8df5f0a3f98824d9b43a6de7b611cdf
Alan <alanznala@163.com>
2019-10-29 19:28:45 +0800

    Add files via upload

--- app/src/main/java/MyFragment/HistoryFragment.java
+++ app/src/main/java/MyFragment/HistoryFragment.java
@@ -2,16 +2,13 @@ package MyFragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 
 import com.example.translator.MainActivity;
 import com.example.translator.R;
